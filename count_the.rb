a59afcd423488ac001424bc6996c90736fc29728
swareham-booth <swareham@chicagobooth.edu>
2021-04-14 19:54:21 +0000

    Count the 100%

--- count_the.rb
+++ count_the.rb
@@ -4,3 +4,9 @@
 #   and finally prints, "'the' appeared x times", where x is an Integer
 
 p "Enter a sentence:"
+sentence = gets.chomp.downcase.gsub(/[^0-9a-z ]/i, '')
+
+split = sentence.split(" ")
+count = split.count("the")
+
+p "'the' appeared " + count.to_s + " times"
