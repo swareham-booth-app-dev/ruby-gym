101bcc5ca8a13452e8e8fd9986cbc2473650af6b
swareham-booth <swareham@chicagobooth.edu>
2021-04-14 20:01:59 +0000

    encoder 100%

--- secret_encoder.rb
+++ secret_encoder.rb
@@ -5,4 +5,17 @@
 
 # Your program should print the encoded message.
 
-p "Enter in the secret you want to encode"
+#p "Enter in the secret you want to encode"
+input = gets.chomp
+
+text = input.gsub("a", "1")
+text = text.gsub("e", "2")
+text = text.gsub("i", "3")
+text = text.gsub("o", "4")
+text = text.gsub("u", "5")
+text = text.gsub("A", "1")
+text = text.gsub("E", "2")
+text = text.gsub("I", "3")
+text = text.gsub("O", "4")
+text = text.gsub("U", "5")
+p text
