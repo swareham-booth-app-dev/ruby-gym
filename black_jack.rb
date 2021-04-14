556d3fca16a977b3d03e2378f7c4f4fb8d8c2113
swareham-booth <swareham@chicagobooth.edu>
2021-04-14 19:54:11 +0000

    Blackjack 100%

--- black_jack.rb
+++ black_jack.rb
@@ -8,3 +8,17 @@
 # For example, given a 11 and 13 as input, the 11 should be 'converted' into a 1 so the total sum will be 14.
 
 p "Enter two number separated by spaces:"
+input = gets.chomp
+split = input.split(" ")
+left = split[0].to_i
+right = split[1].to_i
+
+card_sum = left + right
+if card_sum > 21
+  if left == 11 or right == 11
+    card_sum -= 10
+  else
+    card_sum = 0
+  end
+end
+p card_sum.to_s
