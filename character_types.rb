76c5d840066df5c4f39520cf0eb7a0a1d38e8824
swareham-booth <swareham@chicagobooth.edu>
2021-04-14 23:34:44 +0000

    Char types 100%

--- character_types.rb
+++ character_types.rb
@@ -13,4 +13,11 @@
 #  "Number of spaces in the string is: 3"
 #  "Number of digits in the string is: 4"
 
-p "Enter a sentence:"
+#p "Enter a sentence:"
+text = gets.chomp
+letters = text.gsub(/[^a-zA-Z]/i, "")
+spaces = text.gsub(/[^ ]/i, "")
+digits = text.gsub(/[^0-9]/i, "")
+p "Number of letters in the string is: " + letters.length.to_s
+p "Number of spaces in the string is: " + spaces.length.to_s
+p "Number of digits in the string is: " + digits.length.to_s
