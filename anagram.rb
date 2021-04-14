0cbf93327831153a5bf731a316063b7228438775
swareham-booth <swareham@chicagobooth.edu>
2021-04-14 21:36:17 +0000

    Anagram 100%

--- anagram.rb
+++ anagram.rb
@@ -4,6 +4,18 @@
 # Your job is to write a program that receives two words from the user separated by a comma.
 #  Your program should print "true" if the words are anagrams of each other and "false" if they are not. 
 
+require 'set'
+
 p "Enter two words separated by a comma"
 
-user_words = gets.chomp
+user_words = gets.chomp.gsub(" ", "")
+words = user_words.split(",")
+
+
+
+are_anagrams = words[0].chars.sort == words[1].chars.sort
+if are_anagrams
+  p "true"
+else
+  p "false"
+end
